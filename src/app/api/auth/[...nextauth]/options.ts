4bea991c38584ee700267b2c6380ea2eeccd1fbc
dharmesh.g <dharmesh.g@media.net>
2024-06-05 14:36:05 +0530

    clientSecret updated

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,13 +1,14 @@
 import GoogleProvider from "next-auth/providers/google";
 
 console.log('google:', process.env.clientId);
+console.log('google:', process.env.clientSecret);
 const authOptions = {
   // Configure one or more authentication providers
   providers: [
     // !!! Should be stored in .env file.
     GoogleProvider({
       clientId: process.env.clientId,
-      clientSecret: process.env.clientId,
+      clientSecret: process.env.clientSecret,
     }),
   ],
   secret: `UItTuD1HcGXIj8ZfHUswhYdNd40Lc325R8VlxQPUoR0=`,
